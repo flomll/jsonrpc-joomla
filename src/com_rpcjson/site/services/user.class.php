0df4ad13621ff7ce405602f904908c4a7f0dd66b
Florian Müller <flomll@gmx.at>
2012-03-10 08:14:34 +0100

    Change the interface
    combine the $order_by AND $order parameter and change the function name.

--- src/com_rpcjson/site/services/user.class.php
+++ src/com_rpcjson/site/services/user.class.php
@@ -9,7 +9,7 @@
  *
  * COPYRIGHT
  *
- * Copyright 2012 Stijn Van Campenhout <stijn.vancampenhout@gmail.com>
+ * Copyright 2012 Florian Mueller <florian.mueller@mublasafu.net>
  * 
  * This file is part of JSON-RPC2PHP.
  *
@@ -31,19 +31,50 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
+// Includes some useful functions. It groupes the security and reuse functions.
 require_once (JPATH_COMPONENT.DS.'server'.DS.'helper.class.php');
 
+// Define all Joomla default objects to build a response
+define (TAG_TYPE_JOOMLAERROR,   'jerror');
+define (TAG_TYPE_ARTICLE,       'article');
+define (TAG_TYPE_USERINFO,      'userinfo');
+define (TAG_TYPE_POLL,          'poll');
+define (TAG_TYPE_NEWSFEED,      'newsfeed');
+define (TAG_TYPE_CONTACT,       'contact');
+
+// Define the custom objects to build a response
+define (TAG_TYPE_PIXC,          'pixc');
+define (TAG_TYPE_EVENT,         'event');
+
+/** 
+ * @brief This class implements the service 'user'.
+ *
+ * @description TODO
+ */
 class user {
     
-    public function login ($auth) {
+    public function login ($username, $password) {
         return 'NOT IMPLEMENTED';
     }
     
-    public function logout ($auth) {
+    public function logout () {
         return 'NOT IMPLEMENTED';
     }
 
-    public function get_stories ($start, $limit, $where, $order_by, $order, $username = null, $password = null) {
+    /**
+     * @brief Reads some stories from the database selected by the given parameter.
+     * 
+     * @param integer $start
+     * @param integer $limit
+     * @param string $where
+     * @param string $order
+     *
+     * @param string $username
+     * @param string $password
+     *
+     * @return 
+     */
+    public function get_articles_intro ($start, $limit, $where, $order, $username, $password) {
         // Check inputs
 //         if( $username == null || $password == null ) {
 //             // Kann man irgendwie dem Server sagen, dass er einen error mit 'Invalid parameters' returnen soll?
@@ -52,51 +83,83 @@ class user {
 //  	        return $result;
 //         }
     
-	    // Check the authority of the user. This should be done on every service function.
-//    	    if( rpcjson_helper::login_user($username, $password) < 60 ) {
-//  	        $result->failure = 'Invalid username or password!';
-//  	        return $result;
-//  	    }
-
 	    $result = new stdClass();
-	    $result->type = 'stories';
-	    	    	    
-	    // Encryption test
- 	    //$source = "Dann wollen wir das einmal testen. Denn wenn wir hier alles encrypten, dann ist das sicher besser.";
-	    //$cr = rpcjson_helper::encrypt ( $source );
-	    //echo $res = rpcjson_helper::decrypt ( $cr );
-	    
+	    $result->type = TAG_TYPE_ARTICLE;
+	    	    
+	    // TODO: Add a virtual column 'rtype' to specify the 
+	    //SELECT id,title, IF(sectionid = 1, 'enabled', 'disabled') AS rtype FROM `jos_content` WHERE 1
    	    if( rpcjson_helper::is_joomla15() ) { 
-	        $query = rpcjson_helper::build_db_query( 
+	        $query = rpcjson_helper::build_db_query(
 	        "SELECT #__content.id, ".
 				"	#__content.title, ".
-				"	LEFT(#__content.introtext,100) introtext, ".
+				"	LEFT(#__content.introtext,50) introtext, ".
 				"	#__content.created, ".
 				"	#__content.access, ".
 				"	#__content.state, ".
 				"	#__categories.title category_title, ".
 				"	#__users.name created_by_name ".
+				"   ".
 				"FROM #__content ".
 				"LEFT JOIN #__categories ".
 				"	ON #__categories.id = #__content.catid ".
 				"LEFT JOIN #__users ".
 				"	ON #__users.id = #__content.created_by ",
-				$start, $limit, $where, $order_by, $order);
+				$start, $limit, $where, $order);
 	    }
 	    // FIXME: Add the query code for joomla > 1.5
-	    
+
 	    // Take the query and load the data from the database. This function is secured 
 	    // by password and username. It logs the user in on execution and log out on return.
 	    $result->data = rpcjson_helper::build_payload ($username, $password, $query, 0);
-	    
-	    // Log the user out. This should be done on every service function.
-//	    rpcjson_helper::logout_user();
-	    
+	    	    
 		return $result;
 	}
 	
-	public function get_userinfo ($username = null, $password = null) {
+	/**
+	 */
+	public function get_article ($username, $password, $id) {
 	    return 'NOT IMPLEMENTED';
 	}
+
+	public function get_user ($username, $password) {
+	    return 'NOT IMPLEMENTED';
+	}
+	
+	public function update_user ($username, $password) {
+	    return 'NOT IMPLEMENTED';
+	}
+	
+	public function upload_image ($username, $password, $filename, $filecontents) {
+	    return 'NOT IMPLEMENTED';
+	}
+	
+	public function get_users ($username, $password, $start, $limit, $where, $order) {
+	    return 'NOT IMPLEMENTED';
+	}
+	
+	/**
+	 * @brief Register a new user.
+	 *
+	 * @param string $name Name to display.
+	 * @param string $username Username to register. It must be unique.
+	 * @param string $email E-Mail address of the user.
+	 * @param string $password The password string.
+	 * @param string $password_retype The retyped password string.
+	 * @param boolean $license TRUE if the user accept the license. Otherwise FALSE.
+	 * @param string $code Is a generated token to verify the registration comes from a user.
+	 *
+	 * @return 
+	 */
+	public function register_user ($name, $username, $email, $password, $password_retype, $license, $code) {
+	    return 'NOT IMPLEMENTED';
+	}
+	
+	public function get_event ($username, $password) {
+	    return 'NOT IMPLEMENTED';
+	}
+	
+	public function get_events ($username, $password) {
+	    return 'NOT IMPLEMENTED';
+	}	
 }
 ?>
